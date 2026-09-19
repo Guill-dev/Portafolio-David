18723127b6e5c42f6331b37839bc4196609fa5f0
Guill-dev <hernandezquirozguillermo2@gmail.com>
2026-09-19 00:13:58 -0500

    Botón OSNC agregado

--- src/Shires.jsx
+++ src/Shires.jsx
@@ -5,9 +5,18 @@ function Shires() {
         href="https://www.seshires.com/artiste/mvlhx95b2al4m3obepyfzq59yhj0yn" 
         target="_blank" 
         rel="noopener noreferrer"
-      >
-        MIRA A ESTE ARTISTA EN{"\n"} S.E. SHIRES
+        >
+        MIRA A ESTE ARTISTA EN S.E. SHIRES
       </a>
+
+      <a 
+        href="https://www.seshires.com/artiste/mvlhx95b2al4m3obepyfzq59yhj0yn" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        >
+        MIRA A ESTE ARTISTA EN{"\n"} ORQUESTA SINFONICA N. DE COLOMBIA
+      </a>
+
     </div>
   );
 }
--- src/VideoRow.jsx
+++ src/VideoRow.jsx
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import { VIDEOS } from './videos';
 import Movement from './Movement';
